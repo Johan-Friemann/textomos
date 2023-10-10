29cfaf4d5a21baa097d50daca28f18e6b123645f
Johan Friemann <johan.friemann@chalmers.se>
2023-10-10 15:05:39 +0200

    add correction util

--- tex-ray/x-ray/xray_util.py
+++ tex-ray/x-ray/xray_util.py
@@ -232,3 +232,32 @@ def measure_dark_field(integrate_energy=True):
     dark_field_image = np.zeros((detector_rows, detector_columns))
 
     return dark_field_image
+
+def perform_flat_field_correction(raw_projections, integrate_energy=True):
+    """Perform flat field correction on a given set of raw projections.
+
+    Args:
+        raw_projections(numpy array[float]): A numpy array of the raw X-Ray
+                                             projections. It has the shape 
+                                             (num_projections, detector_rows,
+                                             detector_columns).
+    
+    Keyword args:
+        integrate_energy (bool): If true the energy fluence is measured by the
+                          detector. Photon count is measured if false.
+    
+    Returns:
+        corrected_projections(numpy array[float]): A numpy array of all the 
+                                                   corrected X-Ray projections.
+                                                   It has the shape 
+                                                   (num_projections, 
+                                                    detector_rows,
+                                                    detector_columns).
+    """
+    flat_field_image = measure_flat_field(integrate_energy)
+    dark_field_image = measure_dark_field(integrate_energy)
+
+    corrected_projections = (raw_projections - dark_field_image) / \
+                            (flat_field_image - dark_field_image)
+    
+    return corrected_projections
