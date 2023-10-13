84452f90d6c99f2abb4c33b895f5890a0676b0ab
Johan Friemann <johan.friemann@chalmers.se>
2023-10-13 18:22:38 +0200

    parametrize

--- tex-ray/x-ray/perform_xray_ct.py
+++ tex-ray/x-ray/perform_xray_ct.py
@@ -7,75 +7,115 @@ import tifffile
 from xray_util import *
 
 ################################################################################
-detector_pixel_size = 0.08/1000
+display = True
 
-distance_source_origin = 10/100
+fiber_path = "./tex-ray/fiber.stl"
 
-distance_origin_detector = 6/100
+fiber_elements = [6]
+
+fiber_ratios = [1.0]
+
+fiber_density = 1.8
+
+matrix_path = "./tex-ray/matrix.stl"
+
+matrix_elements = [6, 1, 17, 8]
+
+matrix_ratios = [0.404, 0.481, 0.019, 0.096]
+
+matrix_density = 1.0
+
+output_path = "./tex-ray/reconstruction.tif"
+
+sample_length_unit = "mm"
+
+scanner_length_unit = "cm"
+
+energy_unit = "keV"
+
+detector_pixel_size = 0.008
+
+distance_source_origin = 10
+
+distance_origin_detector = 6
 
 detector_rows = 100
 
 detector_columns = 640
 
+x_ray_energies = [80]
+
+x_ray_counts = [100]
+
 number_of_projections = 100
 
 scanning_angle = 360
 
+projection_angles = np.linspace(0,np.deg2rad(scanning_angle),
+                                num=number_of_projections)
+
 threshold = 0.000000001
+
+reconstruction_algorithm = 'FDK_CUDA'
 ################################################################################
 
 gvxr.createOpenGLContext()
 
 set_up_detector(distance_origin_detector, detector_columns, detector_rows,
-                detector_pixel_size, length_unit="m")
-set_up_xray_source(distance_source_origin, -1, [80], [100])
-
-set_up_sample("./tex-ray/fiber.stl" , [6], [1.0], 1.8, "./tex-ray/matrix.stl",
-              [6, 1, 17, 8], [0.404, 0.481, 0.019, 0.096], 1.0,
-              length_unit="mm")
+                detector_pixel_size, length_unit=scanner_length_unit)
+set_up_xray_source(distance_source_origin, -1, x_ray_energies, x_ray_counts, 
+                   length_unit=scanner_length_unit, energy_unit=energy_unit)
+set_up_sample(fiber_path, fiber_elements, fiber_ratios, fiber_density,
+              matrix_path, matrix_elements, matrix_ratios, matrix_density,
+              length_unit=sample_length_unit)
 raw_projections = perform_tomographic_scan(number_of_projections,
-                                           scanning_angle, display=True)
+                                           scanning_angle, display=display)
 flat_field_image = measure_flat_field()
 dark_field_image = measure_dark_field()
 corrected_projections = perform_flat_field_correction(raw_projections,
                                                       flat_field_image,
                                                       dark_field_image)
 neg_log_projections = neg_log_transform(corrected_projections, threshold)
-################################################################################
 
-# Make sure the data is in single-precision floating-point numbers
-neg_log_projections = np.array(neg_log_projections).astype(np.single)
-
-# Reformat the projections into a set of sinograms
+# Reformat the projections into a set of sinograms on the form that ASTRA needs.
 sinograms = np.swapaxes(neg_log_projections, 0, 1)
 sinograms = np.array(sinograms).astype(np.single)
 
-corrected_projections = None # Not needed anymore
+# Not needed anymore so we remove... It is necessary because emory might run out
+# if we have many detector pixels and a large number of projections.
+raw_projections = None
+flat_field_image = None
+dark_field_image = None
+corrected_projections = None 
 
-angles = np.linspace(0,2*np.pi,num=number_of_projections)
-scale_factor = 1 / (detector_pixel_size * distance_source_origin/(distance_source_origin + distance_origin_detector))
-vol_geom = astra.create_vol_geom(detector_columns, detector_columns, detector_rows)
-proj_geom = astra.creators.create_proj_geom('cone', detector_pixel_size*scale_factor, detector_pixel_size*scale_factor, detector_rows, detector_columns, angles, distance_source_origin*scale_factor, distance_origin_detector*scale_factor)
-
-proj_id = astra.data3d.create('-sino', proj_geom, data=sinograms)
-
-rec_id = astra.data3d.create('-vol', vol_geom, data=0)
-
-alg_cfg = astra.astra_dict('FDK_CUDA')
+scale_factor = compute_astra_scale_factor(distance_source_origin,
+                                          distance_origin_detector,
+                                          detector_pixel_size)
+vol_geo = astra.create_vol_geom(detector_columns, detector_columns,
+                                 detector_rows)
+proj_geo = astra.creators.create_proj_geom('cone',
+                                           detector_pixel_size*scale_factor,
+                                           detector_pixel_size*scale_factor,
+                                           detector_rows,
+                                           detector_columns,
+                                           projection_angles,
+                                           distance_source_origin*scale_factor,
+                                           distance_origin_detector*scale_factor
+                                        )
+proj_id = astra.data3d.create('-sino', proj_geo, data=sinograms)
+rec_id = astra.data3d.create('-vol', vol_geo, data=0)
+alg_cfg = astra.astra_dict(reconstruction_algorithm)
 alg_cfg['ReconstructionDataId'] = rec_id
 alg_cfg['ProjectionDataId'] = proj_id
-
 alg_id = astra.algorithm.create(alg_cfg)
-print('Reconstructing... ')
 astra.algorithm.run(alg_id)
-print('done')
 reconstruction = astra.data3d.get(rec_id)
 
+# Remove eventual erroneous negative values.
 reconstruction[reconstruction < 0] = 0
 
-# Rescale to get attenuation per cm
-reconstruction /= detector_pixel_size * distance_source_origin/(distance_source_origin + distance_origin_detector)
-reconstruction /= 100
+# Rescale to get attenuation coefficient in scanner_length_unit^-1.
+reconstruction /= detector_pixel_size * distance_source_origin/(
+    distance_source_origin + distance_origin_detector)
 
-
-tifffile.imwrite('./tex-ray/test.tif', reconstruction)
+tifffile.imwrite(output_path, reconstruction)
