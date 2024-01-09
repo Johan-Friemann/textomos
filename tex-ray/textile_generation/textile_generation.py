258f212b834e3ecaba4e3132ead7025bc37bbef2
Johan Friemann <johan.friemann@chalmers.se>
2024-01-09 17:39:41 +0100

    working prototype

--- tex-ray/textile_generation/textile_generation.py
+++ tex-ray/textile_generation/textile_generation.py
@@ -1,24 +1,66 @@
+import pymeshlab as pml
 from TexGen.Core import *
 
 
 def create_layer2layer_unit_cell(
-    num_yarns_x,
-    num_yarns_y,
+    cell_x_size,
+    cell_y_size,
+    cell_z_size,
+    num_weft,
+    num_warp,
     num_layers,
+    spacing_ratio,
+    weft_to_warp_ratio,
     weave_pattern,
-    cell_thickness,
-    x_yarn_spacing,
-    y_yarn_spacing,
-    x_yarn_width,
-    y_yarn_width,
-    x_yarn_thickness,
-    y_yarn_thickness,
 ):
+    """Generate a layer to layer fabric unit cell using TexGen.
+
+    Args:
+        cell_x_size (float): The size of the unit cell in the x-direction.
+
+        cell_y_size (float): The size of the unit cell in the y-direction.
+
+        cell_z_size (float): The size of the unit cell in the z-direction.
+
+        num_weft (int): The number of weft yarns per layer.
+
+        num_warp (int): The number of warp yarns per layer.
+
+        num_layers (int): The number of layers (weft yarns have one additional
+                         layer).
+
+        spacing ratio (float): A number between 0 and 1 that determines how wide
+                              the yarns are in relation to the yarn spacing.
+
+        weft_to_warp_ratio (float): A number between 0 and 1 that determines how
+                                   thick weft yarns are in relation to the warp
+                                   yarns.
+                                 
+        weave_pattern (list[list[int]]): A list containing lists of length 3.
+            The list decides which crossing points to "push up" or "push down"
+            in order to generate the textile weave pattern. The first element in
+            the list of length 3 refers to the weft number, the second element
+            refers to the warp number and the third element is 1 for "push up",
+            or -1 for "push down". A "push" will move all the warp yarns at the 
+            selected location either one layer up or down.
+    
+    Keyword args:
+        -
+
+    Returns:
+        Weft (CTextile): A TexGen object that describes the weft yarns.
+
+        Warp (CTextile): A TexGen object that describes the warp yarns.
     """
-    TEMP
-    """
+    x_yarn_spacing = cell_y_size / num_weft
+    y_yarn_spacing = cell_x_size / num_warp
+    x_yarn_width = x_yarn_spacing * spacing_ratio
+    y_yarn_width = y_yarn_spacing * spacing_ratio
+    x_yarn_thickness = cell_z_size * weft_to_warp_ratio / (num_layers + 1)
+    y_yarn_thickness = cell_z_size * (1.0 - weft_to_warp_ratio) / num_layers
+
     # Put dummy variable at spacing, since we overwrite it manually.
-    Textile = CTextileWeave3D(num_yarns_x, num_yarns_y, 1.0, cell_thickness)
+    Textile = CTextileWeave3D(num_warp, num_weft, 1.0, cell_z_size)
 
     Textile.SetXYarnSpacings(x_yarn_spacing)
     Textile.SetYYarnSpacings(y_yarn_spacing)
@@ -49,7 +91,7 @@ def create_layer2layer_unit_cell(
 
     for i in range(Textile.GetNumYarns()):
         Yarn = Textile.GetYarn(i)
-        if i < num_yarns_y * (num_layers + 1):
+        if i < num_weft * (num_layers + 1):
             Weft.AddYarn(Yarn)
         else:
             Warp.AddYarn(Yarn)
@@ -60,8 +102,28 @@ def create_layer2layer_unit_cell(
 def write_layer_to_layer_unit_cell_mesh(
     weft, warp, weft_path, warp_path, matrix_path
 ):
-    """
-    TEMP
+    """Write meshes for the weft, warp, and matrix from TexGen objects
+       representing the weft and warp yarns. The meshes are saved as stl files.
+
+    Args:
+        weft (CTextile): A TexGen Textile object that represents the weft yarns.
+
+        warp (CTextile): A TexGen Textile object that represents the warp yarns.
+
+        weft_path (str): The absolute path (including file name) of where to
+                        write the weft mesh to.
+
+        warp_path (str): The absolute path (including file name) of where to
+                        write the warp mesh to.
+
+        matrix_path(str): The absolute path (including file name) of where to
+                        write the matrix mesh to.
+
+    Keyword args:
+        -
+
+    Returns:
+        -
     """
     weft_mesh = CMesh()
     warp_mesh = CMesh()
@@ -77,34 +139,27 @@ def write_layer_to_layer_unit_cell_mesh(
     matrix_mesh.SaveToSTL(matrix_path, True)
 
 
-weave_pattern = [
-    [1, 0, 1],
-    [3, 0, -1],
-    [5, 1, 1],
-    [7, 1, -1],
-    [3, 2, 1],
-    [1, 2, -1],
-    [7, 3, 1],
-    [5, 3, -1],
-]
-Weft, Warp = create_layer2layer_unit_cell(
-    8,
-    4,
-    6,
-    weave_pattern,
-    4.0,
-    1.625,
-    2.6875,
-    1.625 * 0.9,
-    2.6875 * 0.9,
-    0.3,
-    0.3,
-)
+def boolean_difference_post_processing(weft_path, warp_path):
+    """Use PyMeshLab's boolean difference method to cut the warp yarns out of
+       the weft yarns to resolve mesh overlap. This function overwrites the weft
+       mesh.
 
-write_layer_to_layer_unit_cell_mesh(
-    Weft,
-    Warp,
-    "./tex-ray/weft.stl",
-    "./tex-ray/warp.stl",
-    "./tex-ray/matrix.stl",
-)
+    Args:
+        weft_path (str): The absolute path (including file name) to the weft
+                         mesh.
+
+        warp_path (str): The absolute path (including file name) to the warp
+                         mesh.
+
+
+    Keyword args:
+        -
+
+    Returns:
+        -
+    """
+    mesh_set = pml.MeshSet()
+    mesh_set.load_new_mesh(warp_path)
+    mesh_set.load_new_mesh(weft_path)
+    mesh_set.generate_boolean_difference()
+    mesh_set.save_current_mesh(weft_path)
