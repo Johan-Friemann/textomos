e1ca1f58dfb431ce0347340a0e9783f9cbda406b
Johan Friemann <johan.friemann@chalmers.se>
2024-01-09 18:02:27 +0100

    integrate with main func

--- tex-ray/main.py
+++ tex-ray/main.py
@@ -5,6 +5,12 @@ from textile_generation.textile_generation import *
 ################################################################################
 config_dict = {}
 
+config_dict["weft_path"] = "./tex-ray/meshes/weft.stl"
+
+config_dict["warp_path"] = "./tex-ray/meshes/warp.stl"
+
+config_dict["matrix_path"] = "./tex-ray/meshes/matrix.stl"
+
 config_dict["unit_cell_weft_length"] = 21.5
 
 config_dict["unit_cell_warp_length"] = 6.5
@@ -42,8 +48,6 @@ config_dict["fiber_ratios"] = [1.0]
 
 config_dict["fiber_density"] = 1.8
 
-config_dict["matrix_path"] = "./tex-ray/meshes/matrix.stl"
-
 config_dict["matrix_elements"] = [6, 1, 17, 8]
 
 config_dict["matrix_ratios"] = [0.404, 0.481, 0.019, 0.096]
@@ -67,31 +71,7 @@ config_dict["threshold"] = 0.000000001
 config_dict["reconstruction_algorithm"] = "FDK_CUDA"
 ################################################################################
 
-
-Weft, Warp = create_layer2layer_unit_cell(
-    config_dict["unit_cell_weft_length"],
-    config_dict["unit_cell_warp_length"],
-    config_dict["unit_cell_thickness"],
-    config_dict["weft_yarns_per_layer"],
-    config_dict["warp_yarns_per_layer"],
-    config_dict["number_of_yarn_layers"],
-    config_dict["yarn_width_to_spacing_ratio"],
-    config_dict["weft_to_warp_ratio"],
-    config_dict["weave_pattern"],
-)
-
-write_layer_to_layer_unit_cell_mesh(
-    Weft,
-    Warp,
-    "./tex-ray/meshes/weft.stl",
-    "./tex-ray/meshes/warp.stl",
-    "./tex-ray/meshes/matrix.stl",
-)
-
-boolean_difference_post_processing(
-    "./tex-ray/meshes/weft.stl", "./tex-ray/meshes/warp.stl"
-)
-
+generate_unit_cell(config_dict)
 
 sinograms = generate_sinograms(config_dict)
 reconstruction = perform_tomographic_reconstruction(sinograms, config_dict)
