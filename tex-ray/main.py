722c983a953ccc53e2d0c44a60a14315eb53ad55
Johan Friemann <johan.friemann@chalmers.se>
2024-03-01 15:38:07 +0100

    update main to correct types

--- tex-ray/main.py
+++ tex-ray/main.py
@@ -42,29 +42,25 @@ config_dict["weave_pattern"] = [
 ]
 
 config_dict["deform"] = [
-    10,
-    10,
-    10,
+    10.0,
+    10.0,
+    10.0,
     0.1,
     0.1,
     0.1,
-    5,
-    5,
-    10,
+    5.0,
+    5.0,
+    10.0,
     0.05,
     0.05,
     0.05,
 ]
-
-config_dict["weft_path"] = "./tex-ray/meshes/weft.stl"
 
 config_dict["weft_elements"] = [6]
 
 config_dict["weft_ratios"] = [1.0]
 
 config_dict["weft_density"] = 1.8
-
-config_dict["warp_path"] = "./tex-ray/meshes/warp.stl"
 
 config_dict["warp_elements"] = [6]
 
@@ -78,9 +74,9 @@ config_dict["matrix_ratios"] = [0.404, 0.481, 0.019, 0.096]
 
 config_dict["matrix_density"] = 1.0
 
-config_dict["offset"] = [0, 0, 12]
+config_dict["offset"] = [0.0, 0.0, 12.0]
 
-config_dict["tilt"] = [-1, 2, -3]
+config_dict["tilt"] = [-1.0, 2.0, -3.0]
 
 config_dict["rot_axis"] = "x"
 
@@ -97,9 +93,9 @@ config_dict["energy_unit"] = "keV"
 # 13.5 um --> 0.0135 mm --> (LFOV) --> effective 0.0375 mm
 config_dict["detector_pixel_size"] = 13.5 / 0.4 / 1000.0
 
-config_dict["distance_source_origin"] = 60
+config_dict["distance_source_origin"] = 60.0
 
-config_dict["distance_origin_detector"] = 80
+config_dict["distance_origin_detector"] = 80.0
 
 config_dict["detector_rows"] = 2048
 
@@ -107,7 +103,7 @@ config_dict["detector_columns"] = 2048
 
 config_dict["number_of_projections"] = 801
 
-config_dict["scanning_angle"] = 360
+config_dict["scanning_angle"] = 360.0
 
 config_dict["threshold"] = 0.000000001
 
@@ -117,11 +113,11 @@ config_dict["photonic_noise"] = True
 
 config_dict["binning"] = 4
 
-config_dict["anode_angle"] = 12
+config_dict["anode_angle"] = 12.0
 
-config_dict["tube_voltage"] = 40
+config_dict["tube_voltage"] = 40.0
 
-config_dict["tube_power"] = 10
+config_dict["tube_power"] = 10.0
 
 config_dict["filter_thickness"] = 2.0
 
@@ -129,7 +125,7 @@ config_dict["filter_material"] = "Al"
 
 config_dict["target_material"] = "W"
 
-config_dict["exposure_time"] = 5
+config_dict["exposure_time"] = 5.0
 
 config_dict["energy_bin_width"] = 0.5
 
