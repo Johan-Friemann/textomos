220d69000c34b03b92ef8a69e54150b88bf966ca
Johan Friemann <johan.friemann@chalmers.se>
2024-05-17 11:03:13 +0200

    wrote docstrings

--- tex-ray/hdf5_utils.py
+++ tex-ray/hdf5_utils.py
@@ -1,7 +1,18 @@
+import os
 import tifffile
 import h5py
 import meshio
-import numpy as np
+
+"""
+This file contains routines for saving simulation results to a database of hdf5
+files. The design of the database structure is aimed to counteract two
+anti-patterns of machine learning data: 1 monolithic file and a large amount of
+files containing 1 data point each.
+
+Therefore, the data is split into "chunks" of 100 data points per file. The
+global id of each data point is recorded in a database map file, together
+with the data points metadata.
+"""
 
 
 def save_mesh_to_hdf5(
@@ -9,8 +20,21 @@ def save_mesh_to_hdf5(
     local_idx,
     config_dict,
 ):
-    """
-    DOCSTRING
+    """Save meshes to an hdf5 file.
+
+    Args:
+        chunk_path (str): Path to mesh chunk file.
+
+        local_idx (int): The local index of the data inside the chunk.
+
+        config_dict (dict): A dictionary of Tex-Ray options pertaining the
+                            simulation that is about to be saved.
+
+    Keyword args:
+        -
+
+    Returns:
+        -
     """
     weft_mesh = meshio.read(config_dict["weft_path"])
     weft_vertex_coords = weft_mesh.points
@@ -44,8 +68,21 @@ def save_mesh_to_hdf5(
 
 
 def save_reconstruction_to_hdf5(chunk_path, local_idx, config_dict):
-    """
-    DOCSTRING
+    """Save a reconstruction to an hdf5 file.
+
+    Args:
+        chunk_path (str): Path to reconstruction chunk file.
+
+        local_idx (int): The local index of the data inside the chunk.
+
+        config_dict (dict): A dictionary of Tex-Ray options pertaining the
+                            simulation that is about to be saved.
+
+    Keyword args:
+        -
+
+    Returns:
+        -
     """
     reconstruction = tifffile.imread(config_dict["reconstruction_output_path"])
     with h5py.File(chunk_path, "a") as f:
@@ -55,8 +92,21 @@ def save_reconstruction_to_hdf5(chunk_path, local_idx, config_dict):
 
 
 def save_segmentation_to_hdf5(chunk_path, local_idx, config_dict):
-    """
-    DOCSTRING
+    """Save a segmentation to an hdf5 file.
+
+    Args:
+        chunk_path (str): Path to segmentation chunk file.
+
+        local_idx (int): The local index of the data inside the chunk.
+
+        config_dict (dict): A dictionary of Tex-Ray options pertaining the
+                            simulation that is about to be saved.
+
+    Keyword args:
+        -
+
+    Returns:
+        -
     """
     segmentation = tifffile.imread(config_dict["segmentation_output_path"])
     with h5py.File(chunk_path, "a") as f:
@@ -66,16 +116,56 @@ def save_segmentation_to_hdf5(chunk_path, local_idx, config_dict):
 
 
 def save_data(
-    map_path,
-    mesh_root_path,
-    reconstruction_root_path,
-    segmentation_root_path,
+    database_path,
     config_dict,
 ):
     """
-    DOCSTRING
+    Save a virtual scan and its metadata to an hdf5 database. The database is
+    arranged like:
+
+    /database_folder/
+                     database_map (contains metadata and ids of all data points)
+                     mesh_data/
+                               mesh_data_0.hdf5
+                               ...
+                     reconstruction_data/
+                                        reconstruction_data_0.hdf5
+                                        ...
+                     segmentation_data/
+                                       segmentation_data_0.hdf5
+                                       ...
+
+
+    Args:
+        data_base_path (str): The absolute path to the database folder. If it
+                              doesn't exist it is created (create new database).
+
+        config_dict (dict): A dictionary of Tex-Ray options pertaining the
+                            simulation that is about to be saved.
+
+    Keyword args:
+        -
+
+    Returns:
+        -
     """
-    chunk_size = 2
+    chunk_size = 100  # Magic number, a reasonable chunk size.
+
+    map_path = os.path.join(database_path, "database_map.hdf5")
+    if not os.path.exists(database_path):
+        os.makedirs(database_path)
+
+    mesh_path = os.path.join(database_path, "mesh_data/")
+    if not os.path.exists(mesh_path):
+        os.makedirs(mesh_path)
+
+    reconstruction_path = os.path.join(database_path, "reconstruction_data/")
+    if not os.path.exists(reconstruction_path):
+        os.makedirs(reconstruction_path)
+
+    segmentation_path = os.path.join(database_path, "segmentation_data/")
+    if not os.path.exists(segmentation_path):
+        os.makedirs(segmentation_path)
 
     with h5py.File(map_path, "a") as f:
         # We always increase by 1, -1 hack to make first instance go to 0
@@ -95,32 +185,22 @@ def save_data(
         group.attrs["chunk_index"] = chunk_idx
         group.attrs["local_index"] = local_idx
         sub_group = group.create_group("metadata")
-        sub_group.attrs.update(config_dict)
 
-    mesh_chunk_path = mesh_root_path + "mesh_data_" + str(chunk_idx) + ".hdf5"
+        # We exclude path information from database as it is not relevant.
+        for key in config_dict.keys():
+            if not "path" in key:
+                sub_group.attrs[key] = config_dict[key]
+
+    mesh_chunk_path = mesh_path + "mesh_data_" + str(chunk_idx) + ".hdf5"
     save_mesh_to_hdf5(mesh_chunk_path, local_idx, config_dict)
 
     reconstruction_chunk_path = (
-        reconstruction_root_path + "reconstruction_data_" + str(chunk_idx) + ".hdf5"
+        reconstruction_path + "reconstruction_data_" + str(chunk_idx) + ".hdf5"
     )
     save_reconstruction_to_hdf5(
         reconstruction_chunk_path, local_idx, config_dict
     )
     segmentation_chunk_path = (
-        segmentation_root_path + "segmentation_data_" + str(chunk_idx) + ".hdf5"
+        segmentation_path + "segmentation_data_" + str(chunk_idx) + ".hdf5"
     )
     save_segmentation_to_hdf5(segmentation_chunk_path, local_idx, config_dict)
-
-
-import json
-
-with open("./tex-ray/input/default_input.json") as f:
-    config_dict = json.load(f)
-
-save_data(
-    "./tex-ray/foo.hdf5",
-    "./tex-ray/",
-    "./tex-ray/",
-    "./tex-ray/",
-    config_dict,
-)
