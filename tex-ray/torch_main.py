a668ca278132210699ac682ece09d3fbab8e7e66
Johan Friemann <johan.friemann@chalmers.se>
2024-06-11 14:16:27 +0200

    test model

--- tex-ray/torch_main.py
+++ tex-ray/torch_main.py
@@ -1,11 +1,154 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
 from torchvision.transforms.functional import to_pil_image
 from torchvision.utils import draw_segmentation_masks
+from torchvision.models.segmentation import (
+    deeplabv3_resnet50,
+    DeepLabV3_ResNet50_Weights,
+    deeplabv3_resnet101,
+    DeepLabV3_ResNet101_Weights,
+    fcn_resnet50,
+    FCN_ResNet50_Weights,
+    fcn_resnet101,
+    FCN_ResNet101_Weights,
+)
+from torch_dataset import TexRayDataset, TIFFDataset
+from torch_train import train_model
 
 
-def build_model():
-    """DOCSTRING"""
-    return None
+def build_model(
+    model="deeplabv3_resnet50", input_channels=1, pre_trained=False
+):
+    """Build the segmentation model. The model is adapted to use for segmenting
+       images into 4 distinct classes.
+
+    Args:
+        -
+
+    Keyword args:
+        model (str): The model to build. Can be 'deeplabv3_resnet50',
+                     'deeplabv3_resnet101', 'fcn_resnet_50', or
+                     'fcn_resnet_101'.
+
+        input_channels (int): The number of channels of the input data.
+
+        pre_trained (bool): Will initialize with weights trained on a subset
+                            of COCO. See pytorch documentation.
+
+
+    Returns:
+        segmentation_model (pytorch model): The segmentation model to train/use.
+    """
+    weights = None
+    if model == "deeplabv3_resnet50":
+        if pre_trained:
+            weights = DeepLabV3_ResNet50_Weights.DEFAULT
+        segmentation_model = deeplabv3_resnet50(weights=weights)
+    elif model == "deeplabv3_resnet101":
+        if pre_trained:
+            weights = DeepLabV3_ResNet101_Weights.DEFAULT
+        segmentation_model = deeplabv3_resnet101(weights=weights)
+    elif model == "fcn_resnet50":
+        if pre_trained:
+            weights = FCN_ResNet50_Weights.DEFAULT
+        segmentation_model = fcn_resnet50(weights=weights)
+    elif model == "fcn_resnet101":
+        if pre_trained:
+            weights = FCN_ResNet101_Weights.DEFAULT
+        segmentation_model = fcn_resnet101(weights=weights)
+    else:
+        raise ValueError("Unsupported model: '" + model + "'.")
+
+    # Replace input to accomodate n channel input data.
+    segmentation_model.backbone.conv1 = nn.Conv2d(
+        input_channels,
+        64,
+        kernel_size=(7, 7),
+        stride=(2, 2),
+        padding=(3, 3),
+        bias=False,
+    )
+
+    # Different architectures have different last layer shape.
+    if model in ["fcn_resnet50", "fcn_resnet101"]:
+        last_layer_input_size = 512
+    if model in ["deeplabv3_resnet50", "deeplabv3_resnet101"]:
+        last_layer_input_size = 256
+
+    # Replace output such that there are 4 classes to classify.
+    segmentation_model.classifier[4] = nn.Conv2d(
+        last_layer_input_size, 4, kernel_size=(1, 1), stride=(1, 1)
+    )
+
+    return segmentation_model
+
+
+def build_dataloaders(
+    dataset,
+    batch_size,
+    num_workers,
+    split=[0.8, 0.2],
+    shuffle=True,
+):
+    """Build the model dataloader dictionary.
+
+    Args:
+        dataset (pytorch dataset): The data set to use for constructing the
+                                   loaders.
+
+        batch_Size (int): The dataloader batch size.
+
+        num_workers (int): The number of cpu workers to use while loading.
+
+    Keyword args:
+        split (list[float]): A list of either length 2 or 3 whose entries sum to
+                             1. If length 2: train/validation split, if length 3
+                             train/validation/test split.
+
+        shuffle (bool): Will shuffle the datasets if True.
+
+    Returns:
+        dataloaders (dict[pytorch dataloader]): The dataloader dictionary.
+    """
+
+    if len(split) == 2:
+        training, validation = torch.utils.data.random_split(dataset, split)
+    elif len(split) == 3:
+        training, validation, testing = torch.utils.data.random_split(
+            dataset, split
+        )
+    else:
+        raise ValueError("Dataloader split can only be of length 2 or 3.")
+
+    if sum(split) != 1.0:
+        raise ValueError("Dataloader split must sum to 1.")
+
+    dataloaders = {
+        "training": DataLoader(
+            training,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=num_workers,
+        ),
+        "validation": DataLoader(
+            validation,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=num_workers,
+        ),
+    }
+
+    if len(split) == 3:
+        dataloaders["testing"] = DataLoader(
+            testing,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=num_workers,
+        )
+
+    return dataloaders
 
 
 def draw_image_with_masks(
@@ -69,10 +212,76 @@ def draw_image_with_masks(
 
     return None
 
-def test_model():
+
+def test_model(model, device, dataloader, idx):
     """DOCSTRING"""
+    model.eval()
+
+    iterator = iter(dataloader)
+    for i in range(idx):
+        next(iterator)
+    inputs = next(iterator)
+
+    if type(inputs) is list: # Handle TIFFDataset vs TexRayDataset.
+        inputs = inputs[0]
+
+    inputs = inputs.to(device)
+
+    prediction = model(inputs.unsqueeze(1))["out"]
+
+    draw_image_with_masks(inputs, prediction)
+
     return None
 
 
 if __name__ == "__main__":
-    print("main")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    test_idx = 250
+
+    batch_size = 8
+    num_epochs = 100
+    learn_rate = 0.001
+    weight_decay = 0.001
+    momentum = 0.9
+    transforms = None
+    num_workers = 4
+    state_dict_path = "./tex-ray/state_dict.pt"
+
+    model = build_model()
+    model = model.to(device)
+    train = False
+
+    if train:
+        dataset = TexRayDataset("./tex-ray/dbase", transform=transforms)
+        weight = dataset.get_loss_weights().to(device)
+        dataloaders = build_dataloaders(dataset, batch_size, num_workers)
+
+        criterion = nn.CrossEntropyLoss(weight=weight)
+        optimizer = optim.SGD(
+            model.parameters(),
+            lr=learn_rate,
+            weight_decay=weight_decay,
+            momentum=momentum,
+        )
+        scheduler = optim.lr_scheduler.PolynomialLR(
+            optimizer, total_iters=num_epochs, power=0.9
+        )
+
+        train_model(
+            model,
+            criterion,
+            optimizer,
+            dataloaders,
+            device,
+            num_epochs,
+            scheduler=scheduler,
+            state_dict_path=state_dict_path,
+        )
+
+    model.load_state_dict(torch.load(state_dict_path))
+    test_set = TIFFDataset(
+        "./tex-ray/reconstructions/real_binned_recon.tiff", slice_axis="x"
+    )
+    test_loader = DataLoader(test_set, batch_size=1, shuffle=False, num_workers=1)
+    test_model(model, device, test_loader, test_idx)
