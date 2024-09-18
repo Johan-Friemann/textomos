ee8745cb0c42450a24e8b1e4ef4876057c1f9933
Johan Friemann <johan.friemann@chalmers.se>
2024-09-18 10:49:22 +0200

    some cleanup

--- tex-ray/yarn_attenuation_util.py
+++ tex-ray/yarn_attenuation_util.py
@@ -1,14 +1,10 @@
 import numpy as np
 
 
-def compute_weight_fraction(
+def compute_matrix_atomic_mass_fractions(
     atomic_weights,
     matrix_compounds,
     matrix_compounds_mixing_ratios,
-    matrix_density,
-    fiber_compound,
-    fiber_volume_fraction,
-    fiber_density,
 ):
     """DOCSTRING
 
@@ -30,7 +26,7 @@ def compute_weight_fraction(
         matrix_compounds_mixing_ratios
     )
 
-    matrix_atomic_weight_ratios = np.sum(
+    matrix_atomic_mass_fractions = np.sum(
         matrix_compounds
         * atomic_weights
         * matrix_compounds_mass_fractions[:, np.newaxis]
@@ -38,142 +34,152 @@ def compute_weight_fraction(
         axis=0,
     )
 
-    fiber_compound_total_mass = np.sum(fiber_compound * atomic_weights)
+    return matrix_atomic_mass_fractions
 
-    yarn_atomic_weight_ratios = (
-        np.sum(
-            matrix_compounds
-            * atomic_weights
-            * matrix_compounds_mass_fractions[:, np.newaxis]
-            * matrix_density
-            * (1.0 - fiber_volume_fraction)
-            / matrix_compounds_total_masses[:, np.newaxis],
-            axis=0,
-        )
-        + fiber_compound
+
+def compute_fiber_atomic_mass_fractions(
+    atomic_weights,
+    fiber_compound,
+):
+    """DOCSTRING
+
+    Args:
+        -
+
+    Keyword args:
+        -
+
+    Returns:
+        -
+    """
+    fiber_atomic_mass_fractions = (
+        fiber_compound
         * atomic_weights
-        * fiber_density
-        * fiber_volume_fraction
-        / fiber_compound_total_mass
+        / np.sum(fiber_compound * atomic_weights)
+    )
+
+    return fiber_atomic_mass_fractions
+
+
+def compute_yarn_atomic_mass_fractions(
+    fiber_atomic_mass_fractions,
+    fiber_density,
+    fiber_volume_fraction,
+    matrix_atomic_mass_fractions,
+    matrix_density,
+):
+    """DOCSTRING
+
+    Args:
+        -
+
+    Keyword args:
+        -
+
+    Returns:
+        -
+    """
+    yarn_atomic_mass_fractions = (
+        matrix_atomic_mass_fractions
+        * matrix_density
+        * (1.0 - fiber_volume_fraction)
+        + fiber_atomic_mass_fractions * fiber_volume_fraction * fiber_density
     ) / (
         fiber_volume_fraction * fiber_density
         + (1.0 - fiber_volume_fraction) * matrix_density
     )
 
-    return yarn_atomic_weight_ratios, matrix_atomic_weight_ratios
+    return yarn_atomic_mass_fractions
 
 
-# [C, H, N, O], atomic units
-atomic_weights = [12.011, 1.008, 14.007, 15.999]
+def compute_yarn_density(fiber_density, fiber_volume_fraction, matrix_density):
+    """DOCSTRING
 
-# number of [C, H, N, O], -
-compound_A = [25, 30, 2, 4]
-compound_B1 = [17, 22, 2, 0]
-compound_B2 = [21, 30, 2, 0]
-compound_C = [1, 0, 0, 0]
+    Args:
+        -
 
-# density, g/cm^3
-density_AB = 1.14
-density_C = 1.78
+    Keyword args:
+        -
 
-# fractions, -
-mass_fraction_A_in_AB = 100 / (100 + 68.1)
-mass_fraction_B_in_AB = 68.1 / (100 + 68.1)
+    Returns:
+        -
+    """
+    yarn_density = (
+        fiber_volume_fraction * fiber_density
+        + (1.0 - fiber_volume_fraction) * matrix_density
+    )
+    return yarn_density
 
-# Assume for now
-mass_fraction_B1_in_B = 0.5
-mass_fraction_B2_in_B = 0.5
 
-#############################
-# Compute vol fraction fiber in yarn
-voxel_size = 46.958218  # µm
-a_weft = 7 / 2  # Num pixels
-b_weft = 32 / 2  # Num pixels
+def compute_fiber_volume_fraction(
+    voxel_size, yarn_voxel_area, fiber_diameter, num_fibers_per_yarn
+):
+    """DOCSTRING
 
-a_warp = 7 / 2
-b_warp = 45 / 2
+    Args:
+        -
 
-weft_area = 186 * voxel_size**2  # From imageJ
-warp_area = 320 * voxel_size**2  # From imageJ
-fiber_radius = 5.2 / 2  # µm
-fiber_area = fiber_radius * fiber_radius * np.pi
-num_fiber_weft = 12000.0
-num_fiber_warp = 24000.0
+    Keyword args:
+        -
 
-volume_fraction_C_in_ABC = (num_fiber_weft * fiber_area) / weft_area
+    Returns:
+        -
+    """
+    measured_yarn_area = voxel_size**2 * yarn_voxel_area
+    total_fiber_area = num_fibers_per_yarn * (fiber_diameter / 2) ** 2 * np.pi
 
-# volume_fraction_C_in_ABC = (num_fiber_warp * fiber_area) / warp_area
-print(volume_fraction_C_in_ABC)
-volume_fraction_AB_in_ABC = 1.0 - volume_fraction_C_in_ABC
-#############################
+    return total_fiber_area / measured_yarn_area
 
-# Atomic units
-total_mass_A = sum([atomic_weights[i] * compound_A[i] for i in range(4)])
-total_mass_B1 = sum([atomic_weights[i] * compound_B1[i] for i in range(4)])
-total_mass_B2 = sum([atomic_weights[i] * compound_B2[i] for i in range(4)])
 
-ratios_matrix = [0, 0, 0, 0]
-ratios_yarn = [0, 0, 0, 0]
-for i in range(4):
-    ratios_matrix[i] = (
-        compound_A[i] * atomic_weights[i] * mass_fraction_A_in_AB / total_mass_A
-        + compound_B1[i]
-        * atomic_weights[i]
-        * mass_fraction_B_in_AB
-        * mass_fraction_B1_in_B
-        / total_mass_B1
-        + compound_B2[i]
-        * atomic_weights[i]
-        * mass_fraction_B_in_AB
-        * mass_fraction_B2_in_B
-        / total_mass_B2
+if __name__ == "__main__":
+    # [C, H, N, O], atomic units
+    atomic_weights = np.array([12.011, 1.008, 14.007, 15.999])
+    # number of [C, H, N, O], -
+    matrix_compounds = np.array(
+        [[25, 30, 2, 4], [17, 22, 2, 0], [21, 30, 2, 0]]
+    )
+    fiber_compound = np.array([1, 0, 0, 0])
+    # Amounts of compounds in g
+    matrix_compounds_mixing_ratios = np.array([100.0, 34.05, 34.05])
+    # density in g/cm^3
+    matrix_density = 1.14
+    fiber_density = 1.78
+    # µm
+    voxel_size = 46.958218
+    fiber_diameter = 5.2
+    # num fibers per yarn
+    num_fiber_weft = 12000.0
+    num_fiber_warp = 24000.0
+    # number of voxels covered by yarn
+    weft_voxel_area = 186
+    warp_voxel_area = 320
+
+    fiber_volume_fraction_weft = compute_fiber_volume_fraction(
+        voxel_size, weft_voxel_area, fiber_diameter, num_fiber_weft
+    )
+    fiber_volume_fraction_warp = compute_fiber_volume_fraction(
+        voxel_size, warp_voxel_area, fiber_diameter, num_fiber_weft
+    )
+    matrix_atomic_mass_fractions = compute_matrix_atomic_mass_fractions(
+        atomic_weights, matrix_compounds, matrix_compounds_mixing_ratios
+    )
+    fiber_atomic_mass_fractions = compute_fiber_atomic_mass_fractions(
+        atomic_weights, fiber_compound
+    )
+    weft_atomic_mass_fractions = compute_yarn_atomic_mass_fractions(
+        fiber_atomic_mass_fractions,
+        fiber_density,
+        fiber_volume_fraction_weft,
+        matrix_atomic_mass_fractions,
+        matrix_density,
+    )
+    weft_density = compute_yarn_density(
+        fiber_density, fiber_volume_fraction_weft, matrix_density
+    )
+    warp_density = compute_yarn_density(
+        fiber_density, fiber_volume_fraction_weft, matrix_density
     )
 
-for i in range(4):
-    ratios_yarn[i] = (
-        compound_A[i]
-        * atomic_weights[i]
-        * mass_fraction_A_in_AB
-        * density_AB
-        * volume_fraction_AB_in_ABC
-        / total_mass_A
-        + compound_B1[i]
-        * atomic_weights[i]
-        * mass_fraction_B_in_AB
-        * mass_fraction_B1_in_B
-        * density_AB
-        * volume_fraction_AB_in_ABC
-        / total_mass_B1
-        + compound_B2[i]
-        * atomic_weights[i]
-        * mass_fraction_B_in_AB
-        * mass_fraction_B2_in_B
-        * density_AB
-        * volume_fraction_AB_in_ABC
-        / total_mass_B2
-        + compound_C[i] * density_C * volume_fraction_C_in_ABC
-    ) / (
-        density_AB * volume_fraction_AB_in_ABC
-        + density_C * volume_fraction_C_in_ABC
-    )
+    print(yarn_atomic_mass_fractions)
 
-density_yarn = (
-    density_C * volume_fraction_C_in_ABC
-    + density_AB * volume_fraction_AB_in_ABC
-)
-
-print("Ratios matrix old:")
-print(ratios_yarn)
-
-print("Ratios matrix new:")
-print(
-    compute_weight_fraction(
-        np.array(atomic_weights),
-        np.array([compound_A, compound_B1, compound_B2]),
-        np.array([100, 34.05, 34.05]),
-        1.14,
-        np.array([1,0,0,0]),
-        volume_fraction_C_in_ABC,
-        1.78,
-    )
-)
+    # [0.92979529 0.02244698 0.02246452 0.02529321]
