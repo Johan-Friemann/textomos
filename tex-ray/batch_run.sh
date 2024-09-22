bb80eacc6985f5992fff2eee6723a7e975410c91
Johan Friemann <johan.friemann@chalmers.se>
2024-09-22 18:08:59 +0200

    fixing the mem leak 4

--- tex-ray/batch_run.sh
+++ tex-ray/batch_run.sh
@@ -5,18 +5,20 @@ chunk_size=20
 database_path=$1
 generate_until=$2
 
-echo "Generating data and saving it to database: $database_path"
-echo "Data is generated in batches of: $num_process."
-echo "Data is generated until database has size: $generate_until."
+printf "#####################################################################\n"
+printf "Generating data and saving it to database: $database_path\n"
+printf "Data is generated in batches of size $num_process\n"
+printf "Data is generated until database has size: $generate_until\n"
+printf "#####################################################################\n\n"
 
 while : ; do
-    echo "Starting to process a batch of size $(($num_process))."
+    printf "Starting to process a batch of size $(($num_process)).\n"
     for i in $(seq 0 $(($num_process-1)));
     do
         python3 /tex-ray/generate_config.py $i /tex-ray/input/ >/dev/null 2>&1 &
     done
     wait
-    echo "Finished generating $(($num_process)) config files."
+    printf "Finished generating $(($num_process)) config files.\n"
 
     pids=()
     for i in $(seq 0 $(($num_process-1)));
@@ -38,7 +40,7 @@ while : ; do
         fi
     done
 
-    echo "Sucessfully generated $success/$num_process textile geometries."
+    printf "Sucessfully generated $success/$num_process textile geometries.\n"
 
     for i in $(seq 0 $(($num_process-1))); do
         if [[ "${rets[$i]}" -eq 0 ]]; then
@@ -58,9 +60,9 @@ while : ; do
     done
 
     if [ ! -f /tex-ray/finished ]; then
-        echo "Finished processing $success samples."
+        printf "Finished processing $success samples.\n\n"
     else
-        echo "Reached target database size: $generate_until. Terminating!"
+        printf "Reached target database size: $generate_until. Terminating!\n"
         rm -f /tex-ray/input/finished
         break
     fi
