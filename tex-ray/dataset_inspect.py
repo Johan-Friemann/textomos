af7759a050e6793ed8070767b1c6567811cc8c53
Johan Friemann <johan.friemann@chalmers.se>
2024-11-11 13:29:45 +0100

    utility

--- tex-ray/dataset_inspect.py
+++ tex-ray/dataset_inspect.py
@@ -7,6 +7,11 @@ from torch_segmentation import TexRayDataset, TIFFDataset
 plt.rcParams.update({"text.usetex": True, "font.family": "serif"})
 
 
+"""
+This file contains small utility scripts for inspecting a dataset.
+It includes plotting individual slices and dataset histograms.
+"""
+
 def compute_dataset_histogram(dataloader, bins, lims):
     counts = torch.zeros(bins, dtype=torch.float32)
     iterator = iter(dataloader)
@@ -17,28 +22,35 @@ def compute_dataset_histogram(dataloader, bins, lims):
     return counts
 
 
-def plot_histogram(counts, bins, lims, title, xlabel, ylabel):
+def plot_histogram(counts, bins, lims, title, xlabel, ylabel, scale, savepath):
     edges = torch.arange(lims[0], lims[1], (lims[1] - lims[0]) / (bins + 1))
-    plt.figure(figsize=(4.77, 3.58), layout="constrained")
-    plt.stairs(counts, edges=edges)
+    plt.figure(figsize=(5.00*scale, 3.76*scale), layout="constrained")
+    plt.stairs(counts, edges=edges, fill=True)
     plt.xticks(fontsize=11)
     plt.xlabel(xlabel)
     plt.yticks(fontsize=11)
     plt.ylabel(ylabel)
     plt.ticklabel_format()
     plt.title(title, fontsize=11)
-    # plt.savefig("./tex-ray/line_spread.pdf")
-    plt.show()
+    plt.grid(visible=True, alpha=0.5)
+    plt.savefig(savepath)
 
 
-def plot_slice(dataloader, slice_idx, dx, lims, title, xlabel, ylabel, cbarlabel):
+def plot_slice(
+    dataloader,
+    slice_idx,
+    dx,
+    scale,
+    txt,
+    txtclr,
+    savepath,
+):
     iterator = iter(dataloader)
     for i in range(slice_idx + 1):
         slice = next(iterator)
     if type(slice) is list:  # Deal with TIFFDataset vs TexRayDataset
         slice = slice[0]
-    plt.figure(figsize=(4.77, 3.58), layout="constrained")
-    plt.imshow(slice[0, 0, ...], cmap="gray", vmin=lims[0], vmax=lims[1])
+    plt.figure(figsize=(5.00*scale, 5.00*scale), layout="constrained")
     scalebar = ScaleBar(
         dx=dx,
         units="um",
@@ -46,47 +58,35 @@ def plot_slice(dataloader, slice_idx, dx, lims, title, xlabel, ylabel, cbarlabel
         color="r",
         length_fraction=0.5,
     )
-    plt.colorbar(label=cbarlabel)
+    plt.text(20,25,txt,color=txtclr,backgroundcolor="w")
     plt.gca().add_artist(scalebar)
-    plt.title(title, fontsize=11)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.show()
+    plt.axis("off")
+    plt.savefig(savepath)
 
 
 if __name__ == "__main__":
-
-    inferenece_input_path = "./tex-ray/reconstructions/BAM2x2x3UC_LFOV_AIR_40kV_10W_5s_BIN4_so80mm_od150mm.tiff"
-    test_set = TIFFDataset(inferenece_input_path)
-    test_loader = DataLoader(
-        test_set, batch_size=1, shuffle=False, num_workers=1
-    )
-    """
-    dataset = TexRayDataset("./tex-ray/training_set")
-    dataset = torch.utils.data.Subset(dataset, range(0, len(dataset), 50))
-    test_loader = DataLoader(
-        dataset, batch_size=1, shuffle=False, num_workers=1
-    )
-    """
+    input_path = "./tex-ray/reconstructions"
+    dataset = TIFFDataset(input_path)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=1)
     bins = 10000
-    lims = (0.0001, 0.6)
-    counts = compute_dataset_histogram(test_loader, bins, lims)
+    lims = (1e-6, 0.6)
+    counts = compute_dataset_histogram(dataloader, bins, lims)
     plot_histogram(
         counts,
         bins,
-        lims,
-        "Histogram of experiment reconstruction voxel values",
+        "Histogram of voxel values",
         "Attenuation (1/cm)",
         "Counts (-)",
+        0.75,
+        "./tex-ray/histogram.pdf",
     )
 
     plot_slice(
-        test_loader,
-        255,
+        dataloader,
+        320,
         46.96,
-        lims,
-        "Slice 255 in the $yz$-plane",
-        "$y$ (pixels)",
-        "$z$ (pixels)",
-        "Attenuation (1/cm)",
+        0.48,
+        "$yz$",
+        "g",
+        "./tex-ray/yz_slice.pdf",
     )
